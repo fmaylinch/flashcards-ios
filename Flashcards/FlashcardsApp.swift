ed9c145f46825501fdc74f317693e379b5d4de79
Ferran Maylinch <ferran@yandex-team.ru>
2024-01-07 22:40:20 +0300

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Ferran Maylinch carrasco on 07.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
