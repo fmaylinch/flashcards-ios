7c04dd22fc15778f3983e0f832c80ed35a64024f
Ferran Maylinch <ferran@yandex-team.ru>
2024-01-08 17:14:23 +0300

    List of cards loaded from API

--- Flashcards/ContentView.swift
+++ Flashcards/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var search: String = ""
+    @State private var filteredCards: [Card] = []
+    @StateObject private var cardsFromApi = CardsFromApi()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                List(filteredCards) { card in
+                    VStack {
+                        Text(card.front).font(.title2)
+                        Text(card.back).font(.title3)
+                    }
+                }
+                .searchable(text: $search)
+                .onChange(of: search, initial: false, filterCards)
+                .task {
+                    do {
+                        try await cardsFromApi.fetch()
+                        filterCards()
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    private func filterCards() {
+        if search.isEmpty {
+            filteredCards = cardsFromApi.cards
+            return
+        }
+        filteredCards = cardsFromApi.cards.filter { card in
+            card.back.contains(search)
+        }
     }
 }
 
---
+++ Flashcards/Models.swift
@@ -0,0 +1,44 @@
+//
+//  Models.swift
+//  Flashcards
+//
+//  Created by Ferran Maylinch carrasco on 08.01.2024.
+//
+
+import Foundation
+
+@MainActor
+class CardsFromApi: ObservableObject {
+    
+    @Published var cards: [Card] = []
+    
+    func fetch() async throws {
+        let url = "http://158.160.43.18:3001/cards/list"
+        var request = URLRequest(url: URL(string: url)!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let token = "TOKEN_HERE"
+        request.addValue("Bearer: " + token, forHTTPHeaderField: "Authorization")
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let cardsResponse = try JSONDecoder().decode(CardsResponse.self, from: data)
+        cards = cardsResponse.cards
+    }
+}
+
+struct CardsResponse: Decodable {
+    let cards: [Card]
+}
+
+struct Card: Decodable, Identifiable {
+    
+    let id: String
+    let front: String
+    let back: String
+    let notes: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case front = "front"
+        case back = "back"
+        case notes = "notes"
+    }
+}
