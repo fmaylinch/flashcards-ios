3adc02d078cf47b9fa694ac4b161987834de4e26
Ferran Maylinch <ferran@yandex-team.ru>
2024-01-28 00:29:04 +0300

    Option for response_format = json_object

--- Flashcards/Services/OpenAIService.swift
+++ Flashcards/Services/OpenAIService.swift
@@ -15,10 +15,18 @@ class OpenAIService {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("Bearer " + Constants.openAiToken, forHTTPHeaderField: "Authorization")
         
+        // According to OpenAI docs, when requesting "json_object" format,
+        //   you must also instruct the model to produce JSON yourself via a message.
         let userMessage = GPTMessage(role: "user", content: prompt)
-        let payload = GPTChatPayload(model: "gpt-4", messages: [userMessage], temperature: temperature, top_p: nil)
+        let payload = GPTChatPayload(
+            model: "gpt-4", // gpt-4-turbo-preview (turbo works worse, it seems)
+            messages: [userMessage],
+            response_format: nil, // GPTResponseFormat(type: "json_object")
+            temperature: temperature,
+            top_p: nil)
         request.httpBody = try JSONEncoder().encode(payload)
         
+        print("Making call to OpenAI with payload: \(payload)")
         let (data, _) = try await URLSession.shared.data(for: request)
         print("Response received from GTP: \(String(data: data, encoding: .utf8))")
         let chatResponse = try JSONDecoder().decode(GPTChatResponse.self, from: data)
@@ -34,6 +42,7 @@ class OpenAIService {
 struct GPTChatPayload: Codable {
     let model: String
     let messages: [GPTMessage]
+    let response_format: GPTResponseFormat?
     // OpenAI doesn't recommend setting both temperature and top_p at the same tie
     let temperature: Double? // defaults to 1, it's value from 0 to 2 (2 is more random)
     let top_p: Double? // defaults to 1, 0.1 means top 10% tokens are considered
@@ -51,4 +60,8 @@ struct GPTChatResponse: Codable {
 
 struct GPTChoice: Codable {
     let message: GPTMessage
+}
+
+struct GPTResponseFormat: Codable {
+    let type: String
 }
